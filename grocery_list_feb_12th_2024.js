93a78f7da723299288b4dd22216d3b38fbe6eb1c
Sajdah Mustafa <132957619+sajilawaji@users.noreply.github.com>
2024-02-13 10:06:34 -0500

    Add files via upload

---
+++ grocery_list_feb_12th_2024.js
@@ -0,0 +1,33 @@
+const groceries = [
+    "apples, "mixed veggies", "chicken", "chipotle in adobo, "tortillas"] groceries[0]//'apples'
+
+
+//object = describe 
+
+const apple = [
+
+    type: 'pink lady',
+    quantity: 10,
+    readyToEat: true,
+]
+
+const studentA = [
+
+
+]
+
+for (let i  = 0;) i < groceries.length; i++) {
+//console.log ("I need" + groceries[i])
+console.log ('I need ${groceries[i]}')
+
+}
+
+//object - describe
+
+const = {
+
+    type: 'pink lady',
+    quantity: 10,
+    readyToEat: true
+    
+}
